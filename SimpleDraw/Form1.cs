65e3aad0e9426e6a5f8deefca0385753dda2bfa6
bulletsman04 <bulletsman04@gmail.com>
2018-10-23 16:06:50 +0200

    Refactoring - Part1

--- SimpleDraw/Form1.cs
+++ SimpleDraw/Form1.cs
@@ -18,283 +18,48 @@ namespace SimpleDraw
         {
             
             InitializeComponent();
-            this.Size = new Size((int)(0.65 * System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Width),
-                (int)(0.85 * System.Windows.Forms.Screen.PrimaryScreen.WorkingArea.Height));
+            this.Size = new Size((int)(0.65 * Screen.PrimaryScreen.WorkingArea.Width),
+                (int)(0.85 * Screen.PrimaryScreen.WorkingArea.Height));
 
-            workingArea = new WorkingArea(new Bitmap(pictureBox.Width,pictureBox.Height));
-            pictureBox.Image = workingArea.Bitmap;
+            workingArea = new WorkingArea(new Bitmap(pictureBox.Width,pictureBox.Height),pictureBox);
+            workingArea.RepaintBitmap();
         }
 
         private void pictureBox_MouseClick(object sender, MouseEventArgs e)
         {
-            int x = e.Location.X;
-            int y = e.Location.Y;
-            Point mousePoint = new Point(x, y);
-            NewMethod(sender, e, x, y, mousePoint);
-        }
+            Point mousePoint = new Point(e.Location.X, e.Location.Y);
+            workingArea.HandleMouseClick(e, mousePoint);
 
-        private void NewMethod(object sender, MouseEventArgs e, int x, int y, Point mousePoint)
-        {
-            bool modelChange = workingArea.State.Mode == Mode.Draw;
-
-            if (workingArea.Polygons.Count == 0)
-            {
-                workingArea.Polygons.Add(new Polygon());
-                workingArea.State.CurrentPolygon = workingArea.Polygons[workingArea.Polygons.Count - 1];
-            }
-
-
-            // Sprawdzmy czy nie kliknęliśmy w wierzchołek
-            foreach (var v in workingArea.State.CurrentPolygon.Vertices)
-            {
-                if (v.Rectangle.Contains(mousePoint))
-                {
-                    // ToDo: TO niedobry if jeśli chodzi o obsługę wielu wielokątów
-                    if (workingArea.State.Mode == Mode.Draw)
-                    {
-                        if (workingArea.State.CurrentPolygon.Vertices.Count >= 3 && v == workingArea.State.FirstVertex)
-                        {
-                            modelChange = false;
-                            workingArea.State.Mode = Mode.Edit;
-                            // ToDo: Code repetition - down
-                            Edge newEdge = new Edge((workingArea.State.PrevVertex, workingArea.State.FirstVertex));
-                            workingArea.State.CurrentPolygon.Edges.Add(newEdge);
-                            workingArea.State.FirstVertex.edges = (newEdge, workingArea.State.FirstVertex.edges.right);
-                            workingArea.State.PrevVertex.edges = (workingArea.State.PrevVertex.edges.left, newEdge);
-                            workingArea.RepaintBitmap();
-                            pictureBox.Refresh();
-                        }
-                        else
-                        {
-                            modelChange = false;
-                        }
-                    }
-
-
-                }
-            }
-
-            // Sprawdzamy czy nie kliknęliśmy w krawędź ToDO: wykonać tylko jeśli nie kilknięto w wierzchołek - chociaż tu chyba nieistotne
-            foreach (var edge in workingArea.State.CurrentPolygon.Edges)
-            {
-
-                if (edge.IsPointClose(mousePoint))
-                {
-                    // ToDo: To trzeba potem wyczyścić
-                    workingArea.State.ClickedEdge = edge;
-                    if (e.Button == MouseButtons.Right)
-                    {
-                        HandleRightEdgeClick(sender, e);
-                    }
-
-                    modelChange = false;
-                    break;
-                }
-            }
-
-
-
-            if (modelChange)
-            {
-                Vertex newVertex = new Vertex(x, y);
-                if (workingArea.State.CurrentPolygon.Vertices.Count != 0)
-                {
-                    // ToDo: Code repetition - up
-                    Edge newEdge = new Edge((workingArea.State.PrevVertex, newVertex));
-                    workingArea.State.CurrentPolygon.Edges.Add(newEdge);
-                    newVertex.edges = (newEdge, null);
-                    workingArea.State.PrevVertex.edges = (workingArea.State.PrevVertex.edges.left, newEdge);
-                }
-                workingArea.State.CurrentPolygon.Vertices.Add(newVertex);
-
-                workingArea.RepaintBitmap();
-                pictureBox.Refresh();
-            }
-        }
-
-        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
-        {
-            if (workingArea.Polygons.Count == 0 || workingArea.State.CurrentPolygon.Vertices.Count == 0)
-                return;
-
-            int x = e.Location.X;
-            int y = e.Location.Y;
-            Point mousePoint = new Point(x, y);
-
-            if (workingArea.State.MovedVertex != null)
-            {
-                HandleVertexMove(mousePoint);
-            }
-
-            workingArea.State.MousePosition = mousePoint;
-            workingArea.RepaintBitmap();
             pictureBox.Refresh();
         }
 
-        private void HandleVertexMove(Point mousePoint)
-        {
-            //workingArea.State.MovedVertex.vPoint = mousePoint;
-
-            workingArea.State.MovedVertex.MoveTo = mousePoint;
-            Edge eright = workingArea.State.MovedVertex.edges.right;
-            Vertex vright = workingArea.State.MovedVertex;
-            Edge eleft = workingArea.State.MovedVertex.edges.left;
-            Vertex vleft = workingArea.State.MovedVertex;
-            List<Vertex> movedVertices = new List<Vertex>();
-            movedVertices.Add(vleft);
-            bool changedLeft = true;
-            bool changedRight = true;
-            bool anyAction = false;
-
-            //left && right
-            while (changedLeft || changedRight)
-            {
-                changedLeft = eleft.PreserveRestrictions(vleft, new Vector2D(vleft.vPoint, vleft.MoveTo.Value),true);
-                changedRight = eright.PreserveRestrictions(vright, new Vector2D(vright.vPoint, vright.MoveTo.Value), false);
-
-
-                if (changedLeft)
-                {
-                    vleft = eleft.ends.left;
-                    eleft = vleft.edges.left;
-                    movedVertices.Add(vleft);
-                    anyAction = true;
-                }
-
-                if (changedRight)
-                {
-                    vright = eright.ends.right;
-                    eright = vright.edges.right;
-                    movedVertices.Add(vright);
-                    anyAction = true;
-                }
-
-                //ToDo: I wyczyść MoveTo ?
-                if (anyAction && vleft == vright)
-                    return;
-            }
        
 
-
-            
-
-            foreach (var movedVertex in movedVertices)
-            {
-                movedVertex.vPoint = movedVertex.MoveTo.Value;
-                movedVertex.MoveTo = null;
-            }
+        private void pictureBox_MouseMove(object sender, MouseEventArgs e)
+        {
+            workingArea.HandleMouseMove(e);
         }
+
+      
+
 
         private void pictureBox_MouseDown(object sender, MouseEventArgs e)
         {
-            // ToDo: tymczasowe (chyba)
-            if (workingArea.State.Mode == Mode.Draw)
-                return;
 
-            int x = e.Location.X;
-            int y = e.Location.Y;
-            Point mousePoint = new Point(x, y);
+            Point mousePoint = new Point(e.Location.X, e.Location.Y);
 
-            foreach (var v in workingArea.State.CurrentPolygon.Vertices)
-            {
-                if (v.Rectangle.Contains(mousePoint))
-                {
-                    workingArea.State.MovedVertex = v;
-                }
-            }
+            workingArea.HandleMouseDown(mousePoint);
         }
+
+     
 
         private void pictureBox_MouseUp(object sender, MouseEventArgs e)
         {
-            if (workingArea.State.Mode == Mode.Draw)
-                return;
-            workingArea.State.MovedVertex = null;
+            workingArea.HandleMouseUp();
         }
 
-        private void HandleRightEdgeClick(object sender, MouseEventArgs e)
-        {
-            ContextMenu contextMenu = new ContextMenu();
-            contextMenu.MenuItems.Add("Length Restriction", new EventHandler(HandleLenghtRestrictionClick));
-            contextMenu.MenuItems.Add("Vertical Restriction", new EventHandler(HandleVerticalRestrictionClick));
-            contextMenu.MenuItems.Add("Horizontal Restriction", new EventHandler(HandleHorizontalRestrictionClick));
-            contextMenu.MenuItems.Add("Clear Restriction", new EventHandler((sender1,e1) => workingArea.State.ClickedEdge.Restrictions.Clear()));
-            pictureBox.ContextMenu = contextMenu;
-            pictureBox.ContextMenu.Show(pictureBox,new Point(e.X,e.Y));
-            pictureBox.ContextMenu = null;
-        }
+     
 
-        private void HandleLenghtRestrictionClick( object sender, EventArgs e)
-        {
-            int length = (int)workingArea.State.ClickedEdge.Length;
-            double desiredLength=length;
-
-            LengthWindow lw = new LengthWindow(ref desiredLength);
-            var result = lw.ShowDialog();
-            desiredLength = lw._length;
-
-            if (result == DialogResult.Cancel || desiredLength==0)
-                return;
-
-            AddRestriction(new LengthRestriciton(workingArea.State.ClickedEdge, desiredLength));
-
-            if (desiredLength != length)
-            {
-                workingArea.State.MovedVertex = workingArea.State.ClickedEdge.ends.left;
-                if (workingArea.State.ClickedEdge.Length == desiredLength)
-                    return;
-                Point? moveTo = workingArea.State.ClickedEdge.FindLengthPoint(desiredLength);
-                HandleVertexMove(moveTo.Value);
-                workingArea.State.MovedVertex = null;
-            }
-        }
-
-        private void HandleVerticalRestrictionClick(object sender, EventArgs e)
-        {
-            Edge left = workingArea.State.ClickedEdge.ends.left.edges.left;
-            Edge right = workingArea.State.ClickedEdge.ends.right.edges.right;
-
-            if (left.Restrictions.Count >= 1 && left.Restrictions[0] is VerticalRestriction)
-                return;
-            if (right.Restrictions.Count >= 1 && right.Restrictions[0] is VerticalRestriction)
-                return;
-
-            AddRestriction(new VerticalRestriction(workingArea.State.ClickedEdge));
-            workingArea.State.MovedVertex = workingArea.State.ClickedEdge.ends.left;
-            Point moveTo = new Point(workingArea.State.ClickedEdge.ends.right.vPoint.X, workingArea.State.MovedVertex.vPoint.Y);
-            HandleVertexMove(moveTo);
-            workingArea.State.MovedVertex = null;
-        }
-
-        private void HandleHorizontalRestrictionClick(object sender, EventArgs e)
-        {
-            Edge left = workingArea.State.ClickedEdge.ends.left.edges.left;
-            Edge right = workingArea.State.ClickedEdge.ends.right.edges.right;
-
-            if (left.Restrictions.Count >= 1 && left.Restrictions[0] is HorizontalRestriction)
-                return;
-            if (right.Restrictions.Count >= 1 && right.Restrictions[0] is HorizontalRestriction)
-                return;
-
-            AddRestriction(new HorizontalRestriction(workingArea.State.ClickedEdge));
-
-            workingArea.State.MovedVertex = workingArea.State.ClickedEdge.ends.left;
-            Point moveTo = new Point(workingArea.State.MovedVertex.vPoint.X, workingArea.State.ClickedEdge.ends.right.vPoint.Y);
-            HandleVertexMove(moveTo);
-            workingArea.State.MovedVertex = null;
-        }
-
-        private void AddRestriction(Restriction restriction)
-        {
-           
-
-            if (workingArea.State.ClickedEdge.Restrictions.Count == 0)
-            {
-                workingArea.State.ClickedEdge.Restrictions.Add(restriction);
-            }
-            else
-            {
-                workingArea.State.ClickedEdge.Restrictions[0] = restriction;
-            }
-        }
+        
     }
 }
--- SimpleDraw/Model/WorkingArea.cs
+++ SimpleDraw/Model/WorkingArea.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MyDrawing;
+using System.Windows.Forms;
 
 
 namespace SimpleDraw.Model
@@ -15,12 +16,17 @@ namespace SimpleDraw.Model
         public List<Polygon> Polygons { get; set; }
         public Bitmap Bitmap { get; }
         public DrawingState State { get; }
+        private PictureBox pictureBox;
 
-        public WorkingArea(Bitmap bitmap)
+        public WorkingArea(Bitmap bitmap, PictureBox pictureBox)
         {
             Bitmap = bitmap;
-            State = new DrawingState();;
-            Polygons = new List<Polygon>();
+            State = new DrawingState();
+            ;
+            Polygons = new List<Polygon> {new Polygon()};
+            State.CurrentPolygon = Polygons[Polygons.Count - 1];
+            this.pictureBox = pictureBox;
+            pictureBox.Image = bitmap;
         }
 
         public void RepaintBitmap()
@@ -39,22 +45,23 @@ namespace SimpleDraw.Model
                     gr.FillRectangle(brush, v.Rectangle);
 
                 }
+
                 //Test MyDrawing
                 MyGraphics myGraphics = new MyGraphics(Bitmap);
 
                 foreach (var edge in State.CurrentPolygon.Edges)
                 {
-                    
+
                     gr.DrawLine(thick_pen, edge.ends.left.vPoint, edge.ends.right.vPoint);
                     // ToDo: Te zwolnić, zrobić środek krawędzi i tangens krawędzi do wypozycjonowania oznaczenia
                     Point edgeMiddle = edge.EdgeMiddle;
-                    
+
                     foreach (var edgeRestriction in edge.Restrictions)
                     {
-                        gr.DrawString(edgeRestriction.ToString(),drawFont,drawBrush,edgeMiddle.X,edgeMiddle.Y);
+                        gr.DrawString(edgeRestriction.ToString(), drawFont, drawBrush, edgeMiddle.X, edgeMiddle.Y);
                     }
                 }
-                
+
                 if (State.Mode == Mode.Edit)
                 {
                     gr.DrawLine(thick_pen, State.PrevVertex.vPoint, State.FirstVertex.vPoint);
@@ -74,7 +81,269 @@ namespace SimpleDraw.Model
                 drawBrush.Dispose();
             }
         }
+
+        public void HandleMouseClick(MouseEventArgs e, Point mousePoint)
+        {
+            // Obsługujemy kliknięcie w istniejący wierzchołek
+            if (VertexClicked(mousePoint))
+                return;
+
+            // Sprawdzamy czy nie kliknęliśmy w krawędź 
+            if (EdgeClicked(mousePoint, e))
+                return;
+
+            // Jeśli kliknęliśmy w wolne miejsce
+
+            // Aktualnie max jeden wielokąt
+            if (State.Mode == Mode.Edit)
+                return;
+            Vertex newVertex = new Vertex(mousePoint.X, mousePoint.Y);
+            if (State.CurrentPolygon.Vertices.Count != 0)
+            {
+                // Nowa krawędź
+                Edge newEdge = new Edge((State.PrevVertex, newVertex));
+                State.CurrentPolygon.Edges.Add(newEdge);
+                newVertex.edges = (newEdge, null);
+                State.PrevVertex.edges = (State.PrevVertex.edges.left, newEdge);
+            }
+
+            State.CurrentPolygon.Vertices.Add(newVertex);
+
+            RepaintBitmap();
+        }
+
+        private bool VertexClicked(Point mousePoint)
+        {
+            foreach (var v in State.CurrentPolygon.Vertices)
+            {
+                if (v.Rectangle.Contains(mousePoint))
+                {
+                    // ToDo: TO niedobry if jeśli chodzi o obsługę wielu wielokątów
+                    if (State.Mode == Mode.Draw)
+                    {
+                        if (State.CurrentPolygon.Vertices.Count >= 3 && v == State.FirstVertex)
+                        {
+                            State.Mode = Mode.Edit;
+                            Edge newEdge = new Edge((State.PrevVertex, State.FirstVertex));
+                            State.CurrentPolygon.Edges.Add(newEdge);
+                            State.FirstVertex.edges = (newEdge, State.FirstVertex.edges.right);
+                            State.PrevVertex.edges = (State.PrevVertex.edges.left, newEdge);
+                            RepaintBitmap();
+                        }
+
+                    }
+                    else
+                    {
+                        // ToDo: obsługa usuniecia wierzchołka jeśli PPM
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private bool EdgeClicked(Point mousePoint, MouseEventArgs e)
+        {
+            foreach (var edge in State.CurrentPolygon.Edges)
+            {
+                if (edge.IsPointClose(mousePoint))
+                {
+                    State.ClickedEdge = edge;
+                    if (e.Button == MouseButtons.Right)
+                    {
+                        HandleRightEdgeClick(e);
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private void HandleRightEdgeClick(MouseEventArgs e)
+        {
+            ContextMenu contextMenu = new ContextMenu();
+            contextMenu.MenuItems.Add("Length Restriction", new EventHandler(HandleLenghtRestrictionClick));
+            contextMenu.MenuItems.Add("Vertical Restriction", new EventHandler(HandleVerticalRestrictionClick));
+            contextMenu.MenuItems.Add("Horizontal Restriction", new EventHandler(HandleHorizontalRestrictionClick));
+            contextMenu.MenuItems.Add("Clear Restriction",
+                new EventHandler((sender1, e1) => State.ClickedEdge.Restrictions.Clear()));
+            pictureBox.ContextMenu = contextMenu;
+            pictureBox.ContextMenu.Show(pictureBox, new Point(e.X, e.Y));
+            pictureBox.ContextMenu = null;
+        }
+
+        private void HandleLenghtRestrictionClick(object sender, EventArgs e)
+        {
+            int length = (int) State.ClickedEdge.Length;
+            double desiredLength = length;
+
+            LengthWindow lw = new LengthWindow(ref desiredLength);
+            var result = lw.ShowDialog();
+            desiredLength = lw._length;
+
+            if (result == DialogResult.Cancel || desiredLength == 0)
+                return;
+
+            AddRestriction(new LengthRestriciton(State.ClickedEdge, desiredLength));
+
+            if (desiredLength != length)
+            {
+                State.MovedVertex = State.ClickedEdge.ends.left;
+                if (State.ClickedEdge.Length == desiredLength)
+                    return;
+                Point? moveTo = State.ClickedEdge.FindLengthPoint(desiredLength);
+                HandleVertexMove(moveTo.Value);
+                State.MovedVertex = null;
+            }
+        }
+
+        private void HandleVerticalRestrictionClick(object sender, EventArgs e)
+        {
+            Edge left = State.ClickedEdge.ends.left.edges.left;
+            Edge right = State.ClickedEdge.ends.right.edges.right;
+
+            if (left.Restrictions.Count >= 1 && left.Restrictions[0] is VerticalRestriction)
+                return;
+            if (right.Restrictions.Count >= 1 && right.Restrictions[0] is VerticalRestriction)
+                return;
+
+            AddRestriction(new VerticalRestriction(State.ClickedEdge));
+            State.MovedVertex = State.ClickedEdge.ends.left;
+            Point moveTo = new Point(State.ClickedEdge.ends.right.vPoint.X, State.MovedVertex.vPoint.Y);
+            HandleVertexMove(moveTo);
+            State.MovedVertex = null;
+        }
+
+        private void HandleHorizontalRestrictionClick(object sender, EventArgs e)
+        {
+            Edge left = State.ClickedEdge.ends.left.edges.left;
+            Edge right = State.ClickedEdge.ends.right.edges.right;
+
+            if (left.Restrictions.Count >= 1 && left.Restrictions[0] is HorizontalRestriction)
+                return;
+            if (right.Restrictions.Count >= 1 && right.Restrictions[0] is HorizontalRestriction)
+                return;
+
+            AddRestriction(new HorizontalRestriction(State.ClickedEdge));
+
+            State.MovedVertex = State.ClickedEdge.ends.left;
+            Point moveTo = new Point(State.MovedVertex.vPoint.X, State.ClickedEdge.ends.right.vPoint.Y);
+            HandleVertexMove(moveTo);
+            State.MovedVertex = null;
+        }
+
+        private void AddRestriction(Restriction restriction)
+        {
+            if (State.ClickedEdge.Restrictions.Count == 0)
+            {
+                State.ClickedEdge.Restrictions.Add(restriction);
+            }
+            else
+            {
+                State.ClickedEdge.Restrictions[0] = restriction;
+            }
+        }
+
+        private void HandleVertexMove(Point mousePoint)
+        {
+            State.MovedVertex.MoveTo = mousePoint;
+            Edge eright = State.MovedVertex.edges.right;
+            Vertex vright = State.MovedVertex;
+            Edge eleft = State.MovedVertex.edges.left;
+            Vertex vleft = State.MovedVertex;
+            List<Vertex> movedVertices = new List<Vertex>();
+            movedVertices.Add(vleft);
+            bool changedLeft = true;
+            bool changedRight = true;
+            bool anyAction = false;
+
+            //left && right
+            while (changedLeft || changedRight)
+            {
+                changedLeft = eleft.PreserveRestrictions(vleft, new Vector2D(vleft.vPoint, vleft.MoveTo.Value), true);
+                changedRight = eright.PreserveRestrictions(vright, new Vector2D(vright.vPoint, vright.MoveTo.Value), false);
+
+
+                if (changedLeft)
+                {
+                    vleft = eleft.ends.left;
+                    eleft = vleft.edges.left;
+                    movedVertices.Add(vleft);
+                    anyAction = true;
+                }
+
+                if (changedRight)
+                {
+                    vright = eright.ends.right;
+                    eright = vright.edges.right;
+                    movedVertices.Add(vright);
+                    anyAction = true;
+                }
+
+                //ToDo: I wyczyść MoveTo ?
+                if (anyAction && vleft == vright)
+                    return;
+            }
+
+
+
+
+
+            foreach (var movedVertex in movedVertices)
+            {
+                movedVertex.vPoint = movedVertex.MoveTo.Value;
+                movedVertex.MoveTo = null;
+            }
+        }
+
+        public void HandleMouseMove(MouseEventArgs e)
+        {
+            if (Polygons.Count == 0 || State.CurrentPolygon.Vertices.Count == 0)
+                return;
+
+            int x = e.Location.X;
+            int y = e.Location.Y;
+            Point mousePoint = new Point(x, y);
+
+            if (State.MovedVertex != null)
+            {
+                HandleVertexMove(mousePoint);
+            }
+
+            State.MousePosition = mousePoint;
+            RepaintBitmap();
+            pictureBox.Refresh();
+        }
+
+        public void HandleMouseDown(Point mousePoint)
+        {
+            // ToDo: tymczasowe (chyba)
+            if (State.Mode == Mode.Draw)
+                return;
+
+
+
+            foreach (var v in State.CurrentPolygon.Vertices)
+            {
+                if (v.Rectangle.Contains(mousePoint))
+                {
+                    State.MovedVertex = v;
+                }
+            }
+        }
+
+        public void HandleMouseUp()
+        {
+            if (State.Mode == Mode.Draw)
+                return;
+            State.MovedVertex = null;
+        }
+
     }
 
-   
+
 }
